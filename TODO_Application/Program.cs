ed35a1d20d5e00bf7eb1f05e7c0e73117d68722e
hemory <hphife@yahoo.com>
2020-09-19 17:08:08 -0400

    First Commit

---
+++ TODO_Application/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TODO_Application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task task = null;
+
+            List<Task> taskList = new List<Task>();
+
+
+            Console.WriteLine("Welcome to the TODO APP! Add Task or enter (Q) to exit application.");
+
+            int incrementer = 1;
+
+            while (true)
+            {
+                task = new Task {TaskNumber = incrementer};
+
+                Console.Write($"{task.TaskNumber}:  ");
+                task.TaskItem = Console.ReadLine();
+
+                if (task.TaskItem != null && task.TaskItem.ToLower() == "q")
+                {
+                    break;
+                }
+
+                taskList.Add(task);
+
+                incrementer++;
+
+
+
+
+            }
+
+            Console.Clear();
+
+            Console.WriteLine("Your complete list: ");
+            foreach (var item in taskList)
+            {
+                Console.WriteLine($"{item.TaskNumber}: {item.TaskItem}");
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TODO_Application/Task.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TODO_Application
+{
+   public class Task
+    {
+        public int TaskNumber { get; set; }
+        public string TaskItem { get; set; }
+
+    }
+}
